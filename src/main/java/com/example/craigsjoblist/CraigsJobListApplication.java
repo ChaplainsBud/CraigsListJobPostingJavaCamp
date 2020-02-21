48286f482fedb915524d3c9356fed79e1537e883
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-21 08:41:58 -0500

    8:41 a.m. commit

---
+++ src/main/java/com/example/craigsjoblist/CraigsJobListApplication.java
@@ -0,0 +1,13 @@
+package com.example.craigsjoblist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraigsJobListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraigsJobListApplication.class, args);
+    }
+
+}
