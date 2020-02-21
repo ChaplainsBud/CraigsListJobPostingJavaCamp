5b3030aeda31290f410290b889c2ed4137a1de81
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-21 13:14:40 -0500

    Working on design

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,8 @@
+console.log("It works");
+
+// let btn = document.querySelector('#practice');
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     alert("why does it become null?")
+//    console.log("it works again.")
+// });
